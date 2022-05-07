84767ae98d4c60d3a3641efd36a745b630d8a0a7
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2022-05-07 09:29:27 +0900

    Update generate-constants-rb-from-h.rb

--- DigitalSynthVRA8U/generate-constants-rb-from-h.rb
+++ DigitalSynthVRA8U/generate-constants-rb-from-h.rb
@@ -1,4 +1,4 @@
-File.open("../DigitalSynthVRA8Q/constants.h", "r") do |input|
+File.open("../DigitalSynthVRA8U/constants.h", "r") do |input|
   File.open("./constants.rb", "w") do |output|
     input.each_line do |line|
       output.puts line.chomp[16..-2]
