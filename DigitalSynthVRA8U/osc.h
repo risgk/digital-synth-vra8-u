a90aa33870bd934ed6ab853a97a127e0580f79ff
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2022-06-23 07:34:11 +0900

    Modifying Mono Mode

--- DigitalSynthVRA8U/osc.h
+++ DigitalSynthVRA8U/osc.h
@@ -531,10 +531,12 @@ private:
     uint16_t freq_div_2 = (m_freq_temp[N] >> 1);
     uint8_t bit = (m_rnd >= 0xF0);
     uint8_t mono_offset = 0;
-    if (N == 2) {
-      mono_offset += m_mono_mode;
+    if ((m_mono_mode) && (N == 2)) {
+      if (m_freq[0] == m_freq_temp[2]) {
+        mono_offset = 1; // TODO
+      }
     }
-    int8_t freq_offset = high_sbyte(freq_div_2 * g_osc_tune_table[fine >> (8 - OSC_TUNE_TABLE_STEPS_BITS)]) + bit + mono_offset; // TODO: mono_offset
+    int8_t freq_offset = high_sbyte(freq_div_2 * g_osc_tune_table[fine >> (8 - OSC_TUNE_TABLE_STEPS_BITS)]) + bit - mono_offset;
     m_freq[N] = m_freq_temp[N] + freq_offset;
     m_wave_table[N] = m_wave_table_temp[N];
   }
